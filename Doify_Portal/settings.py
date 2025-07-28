0a88a1fdc9b38c09ef8f443dcd35f38fd0fbed39
Vasudev Pareek <vasudevpareek626@gmail.com>
2025-07-29 00:08:07 +0530

    Try again to connect with render.com

--- Doify_Portal/settings.py
+++ Doify_Portal/settings.py
@@ -77,26 +77,27 @@ WSGI_APPLICATION = 'Doify_Portal.wsgi.application'
 
 import dj_database_url
 import os
+load_dotenv()
 
 DATABASES = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',    
-        'NAME': 'DoifyDevDB',  
-        'USER': 'postgres',      
-        'PASSWORD': 'admin',  
-        'HOST': 'localhost',         
-        'PORT': '5432',               
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',    
+    #     'NAME': 'DoifyDevDB',  
+    #     'USER': 'postgres',      
+    #     'PASSWORD': 'admin',  
+    #     'HOST': 'localhost',         
+    #     'PORT': '5432',               
+    # }
 
-    # 'default': dj_database_url.config(
-    #     default=os.environ.get('DATABASE_URL'),
-    #     conn_max_age=600,
-    #     conn_health_checks=True
-    # )
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
