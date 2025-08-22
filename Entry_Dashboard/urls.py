5fe1bb7afe20e4e5363cef7f7facfebce45ace7a
Vasudev Pareek <vasudevpareek626@gmail.com>
2025-08-22 23:28:43 +0530

    login and signup page updated

--- Entry_Dashboard/urls.py
+++ Entry_Dashboard/urls.py
@@ -3,4 +3,6 @@ from django.urls import path, include
 from .views import *
 urlpatterns = [
     path('', Entery_Welcome_Dashboard_View, name="Entery_Welcome_Dashboard_View"),
+    path('User_Registration', Registration_View, name="User_Registration"),
+    path('User_Login', Login_View, name="User_Login"),
 ]
--- Entry_Dashboard/views.py
+++ Entry_Dashboard/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def Entery_Welcome_Dashboard_View(request):
     return render(request, 'Entry_Dashboard/Entry_Welcome_Dashboard.html')
+
+def Registration_View(request):
+    return render(request, 'Entry_Dashboard/Registeration_Page.html')
+
+def Login_View(request):
+    return render(request, 'Entry_Dashboard/Login_Page.html')
+
+
+# C:\Users\vasudev pareek\Desktop\Doify\Entry_Dashboard\templates\Entry_Dashboard\Registeration_Page.html
